b439d4db428a87ffae80123535580cc9469e3aa5
Said Masoud <saidmasoud@users.noreply.github.com>
2020-09-24 11:50:08 -0400

    comment out

--- helm.sh
+++ helm.sh
@@ -37,10 +37,10 @@ fi
 export HELM_HOME="$(pwd)/.helm"
 export PATH="$(dirname $BINARY):$PATH"
 
-pwd
-echo "Build working dir: ${BUILD_WORKING_DIRECTORY}"
+# pwd
+# echo "Build working dir: ${BUILD_WORKING_DIRECTORY}"
 cd "${BUILD_WORKING_DIRECTORY:-}"
-pwd
+# pwd
 
 # cd to workspace root
 # while [ $PWD != "/" ]; do
